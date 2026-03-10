b580fc7289a2744ab64821517414e518e416b897
rafalpilaczynski-coder <rafal.pilaczynski@gmail.com>
2026-03-10 17:33:21 +0100

    changes in welcome_screen

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,11 +1,33 @@
 // lib/screens/welcome_screen.dart
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import '../shared/primary_button.dart';
+import '../providers/tasting_provider.dart'; // Wymagane dla dostępu do zasobów
 
-class WelcomeScreen extends StatelessWidget {
+class WelcomeScreen extends ConsumerStatefulWidget {
   const WelcomeScreen({super.key});
-  
+
+  @override
+  ConsumerState<WelcomeScreen> createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends ConsumerState<WelcomeScreen> {
+  @override
+  void initState() {
+    super.initState();
+    
+    // INŻYNIERIA WYDAJNOŚCI: Inicjalizacja zasobów w tle.
+    // addPostFrameCallback odpala się dokładnie po narysowaniu pierwszej klatki tego ekranu.
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      // Wymusza pobranie i zdekodowanie obrazów do RAM (ui.Image)
+      ref.read(iconCacheProvider);
+      // Wymusza parsowanie plików JSON z assets
+      ref.read(grindersDatabaseProvider);
+      ref.read(staticRoasteriesProvider); 
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,10 +35,10 @@ class WelcomeScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-           Image.asset(
-            'assets/images/logo.png',
-            height: 160, // Rozmiar optymalny dla proporcji ekranu
-            fit: BoxFit.contain,
+            Image.asset(
+              'assets/images/logo.png',
+              height: 160, 
+              fit: BoxFit.contain,
             ),
             const SizedBox(height: 50),
             
@@ -54,10 +76,10 @@ class WelcomeScreen extends StatelessWidget {
               child: Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 40.0),
                 child: TextButton.icon(
-                  icon: const Icon(Icons.tune, size: 20), // Ikona suwaków (inżynieryjny vibe)
+                  icon: const Icon(Icons.tune, size: 20),
                   label: const Text('PERSONAL SETTINGS', style: TextStyle(fontWeight: FontWeight.bold)),
                   style: TextButton.styleFrom(
-                    foregroundColor: const Color(0xFFC27D56), // Nasz miedziany akcent
+                    foregroundColor: const Color(0xFFC27D56), 
                   ),
                   onPressed: () => context.push('/settings'),
                 ),
