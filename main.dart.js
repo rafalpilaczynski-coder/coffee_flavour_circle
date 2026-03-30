164635e435fcbb9f2d8eae5f8bc407b7943a9183
rafalpilaczynski-coder <rafalpilaczynski-coder@users.noreply.github.com>
2026-03-30 11:26:00 +0000

    deploy: 01a872d90bd13c72d53e4d9d0fd0de08c4f83052

--- main.dart.js
+++ main.dart.js
@@ -32625,7 +32625,7 @@ $S:2}
 A.aJT.prototype={
 $1(a){var s=A.dT().b
 s=s==null?null:s.canvasKitBaseUrl
-return(s==null?"https://www.gstatic.com/flutter-canvaskit/e4b8dca3f1b4ede4c30371002441c88c12187ed6/":s)+a},
+return(s==null?"https://www.gstatic.com/flutter-canvaskit/425cfb54d01a9472b3e81d9e76fd63a4a44cfbcb/":s)+a},
 $S:51}
 A.SH.prototype={
 gA(a){var s=this.a
